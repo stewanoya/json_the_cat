6a1795b9a942e1db4b54196eb3eed8afdc4b5d72
stew anoya <stewartanoya@gmail.com>
2021-11-04 20:48:10 +0000

    Using the cats API to fetch breed description

---
+++ breedFetcher.js
@@ -0,0 +1,22 @@
+const request = require("request");
+const args = process.argv.slice(2);
+
+const fetcher = (breed) => {
+  request(
+    `https://api.thecatapi.com/v1/breeds/search?q=${breed}`,
+    (error, response, body) => {
+      if (error) {
+        console.log(`There was an error. See for yourself -> ${error}`);
+      }
+      let bodyObj = JSON.parse(body);
+      if (bodyObj === []) {
+        return console.log(`Item was not found`);
+      } else {
+        let result = `Here is a description of ${breed} cats: ${bodyObj[0].description}`;
+        return result;
+      }
+    }
+  );
+};
+
+fetcher(args);
